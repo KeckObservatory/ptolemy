f5f584a0108c84b8c38e91c854a2d3e5d8347d6b
tylertucker202 <tylertucker202@gmail.com>
2022-08-16 09:10:08 -1000

    for ddoiserver

--- src/contexts/socket.tsx
+++ src/contexts/socket.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {io} from "socket.io-client";
 
-const IS_PRODUCTION: boolean = process.env.REACT_APP_ENVIRONMENT==='production'? true : false
+const IS_PRODUCTION: boolean = process.env.REACT_APP_ENVIRONMENT==='production'
 console.log(`is PRODUCTION? set to ${IS_PRODUCTION}`) 
-const ENDPOINT = "ws://127.0.0.1:5000"
-export const socket = io(ENDPOINT, {transports: ['websocket'], secure: true, rejectUnauthorized: false});
+const url = IS_PRODUCTION? "https://vm-appserver.keck.hawaii.edu": "localhost:50007" 
+const path = IS_PRODUCTION? '/ex_eng_ws_app' : ''
+export const socket = io(url, {path: path, transports: ['websocket'], secure: true, rejectUnauthorized: false});
 export const SocketContext = React.createContext(socket);
