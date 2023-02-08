8ed8ebd90b830de1c788902fab379e4f82361d7e
tylertucker202 <tylertucker202@gmail.com>
2023-02-08 06:17:48 -1000

    for the server

--- src/contexts/socket.tsx
+++ src/contexts/socket.tsx
@@ -4,8 +4,9 @@ import {io} from "socket.io-client";
 const IS_PRODUCTION: boolean = process.env.REACT_APP_ENVIRONMENT==='production'
 console.log(`is PRODUCTION? set to ${IS_PRODUCTION}`) 
 //const url = IS_PRODUCTION? "vm-appserver.keck.hawaii.edu:50007": "localhost:50007" 
-const url = IS_PRODUCTION? "http://vm-ddoiserverbuild.keck.hawaii.edu": "localhost:50007" 
-const path = IS_PRODUCTION? '/wsapp' : ''
+//const url = IS_PRODUCTION? "http://vm-ddoiserverbuild.keck.hawaii.edu": "localhost:50007" 
+const url = IS_PRODUCTION? "http://vm-ddoiserverbuild.keck.hawaii.edu:50007": "localhost:50007" 
+const path = IS_PRODUCTION? '' : ''
 
 console.log('socket url', url)
 export const socket = io(url, {path: path, transports: ['websocket'], secure: true, rejectUnauthorized: false});
