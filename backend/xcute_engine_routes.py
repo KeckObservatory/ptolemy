6c9b88e69ed7dcc27810bb7491ff0a1025e448d9
Tyler Tucker <tylertucker202@gmail.com>
2023-02-09 13:29:57 -1000

    dependency cleanup

--- backend/xcute_engine_routes.py
+++ backend/xcute_engine_routes.py
@@ -1,13 +1,7 @@
-from app import app, socketio
-from flask_socketio import emit, disconnect 
-from flask import session, send_from_directory, request, copy_current_request_context
-from threading import Lock
-import shelve
-from obdm import OBDM
-import os
+from app import socketio
+from flask_socketio import emit 
 import json
 import logging
-import pdb
 
 from execution_engine.core.ExecutionEngine import ExecutionEngine 
 from execution_engine.core.Queues.BaseQueue import DDOIBaseQueue
