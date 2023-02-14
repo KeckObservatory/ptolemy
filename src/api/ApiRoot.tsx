a210168d4b99f585e6d1f8d78fca19c55982f2c1
Tyler Tucker <tylertucker202@gmail.com>
2023-02-14 13:35:04 -1000

    using test datatbase

--- src/api/ApiRoot.tsx
+++ src/api/ApiRoot.tsx
@@ -24,13 +24,13 @@ const IS_BUILD = IS_PRODUCTION || IS_DEVELOPMENT
 
 console.log(`is BUILD ? set to ${IS_BUILD}`)
 console.log(`is DEVELOPMENT ? set to ${IS_DEVELOPMENT}`)
-var DEVELOPMENT_URL = 'https://www3.keck.hawaii.edu/'
+var DEVELOPMENT_URL = 'https://www3build.keck.hawaii.edu/'
 var PRODUCTION_URL = 'https://www3.keck.hawaii.edu/'
 var TEST_URL = 'http://localhost:50007/v0' //use locally or for testing (npm start or npm run demobuild)
 var BASE_URL = IS_BUILD ? PRODUCTION_URL : TEST_URL // sets for production vs test 
 
 BASE_URL = IS_DEVELOPMENT ? DEVELOPMENT_URL : BASE_URL
-var API_URL = BASE_URL + '/api/ddoi/'
+var API_URL = IS_DEVELOPMENT ? BASE_URL + '/api/test/ddoi/' : BASE_URL + '/api/ddoi/'
 
 var OB_URL = API_URL + 'obsBlocks'
 var CONTAINER_URL = API_URL + 'containers'
