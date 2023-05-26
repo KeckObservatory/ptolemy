37c0c043515953710cbf29cf27b94a9d8f0f43c1
tylertucker202 <tylertucker202@gmail.com>
2023-05-25 15:25:47 -1000

    debugging fix

--- backend/logger_routes.py
+++ backend/logger_routes.py
@@ -3,6 +3,7 @@ import configparser
 from app import app, socketio
 import requests 
 from flask import send_from_directory, request
+import pdb
 
 cfg_name="./cfg.ini"
 config_parser = configparser.ConfigParser()
@@ -13,7 +14,8 @@ def get_logs():
     print('logs requested')
     print('request.args', request.args)
     url = config_parser["URLS"]["logger_url"]
+    url += '/api/log/get_logs'
     print('querying url:', url)
-    resp = requests.get(url, params=request.args)
+    resp = requests.get(url, params=dict(request.args))
     logs = resp.json()
     return 200, logs 
