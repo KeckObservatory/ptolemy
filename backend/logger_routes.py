dcf845d228b84c09d092cd544e1c4f42b976cf8c
Tyler Tucker <tylertucker202@gmail.com>
2023-05-26 08:22:09 -1000

    logger route uses zmq

--- backend/logger_routes.py
+++ backend/logger_routes.py
@@ -4,6 +4,7 @@ from app import app, socketio
 import requests 
 from flask import send_from_directory, request
 import pdb
+from DDOILoggerClient import getlogz_functions as glf 
 
 cfg_name="./cfg.ini"
 config_parser = configparser.ConfigParser()
@@ -16,6 +17,13 @@ def get_logs():
     url = config_parser["URLS"]["logger_url"]
     url += '/api/log/get_logs'
     print('querying url:', url)
-    resp = requests.get(url, params=dict(request.args))
-    logs = resp.json()
+    params = dict(request.args)
+    logs = glf.get_logz(
+        params.get('subsystem', None),
+        params.get('minutes', None),
+        params.get('startDate', None),
+        params.get('endtDate', None),
+        params.get('nLogs', None),
+        params.get('dateFormat', '%Y-%m-%dT%H-%M-%S'),
+                 )
     return 200, logs 
