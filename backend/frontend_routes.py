e3a7cfddbfa4129be8bd1115088dd4ec6cd01c29
Tyler Tucker <tylertucker202@gmail.com>
2023-06-27 09:35:19 -1000

    logger for ptolemy and ee

--- backend/frontend_routes.py
+++ backend/frontend_routes.py
@@ -28,7 +28,7 @@ def create_logger(fileName='ptolemy.log', subsystem="PTOLEMY", author='xxxx', pr
         logger.addHandler(zmq_log_handler)
     except Exception as err:
         print('zmq log handler failed. not going to add')
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logger.INFO)
     return logger
 
 exen_logger = create_logger(subsystem='EXECUTION_ENGINE')
@@ -50,7 +50,7 @@ def index():
 @socketio.on('request_ob')
 def request_ob():
     """Sends OB stored on EE (first item in queue)"""
-    logging.info('sending ob request recieved')
+    logger.info('sending ob request recieved')
     ob_ids = ee.obs_q.get_ob_ids() 
     if len(ob_ids) > 0:
         try: 
@@ -58,7 +58,7 @@ def request_ob():
             ob = ee.ODBInterface.get_OB_from_id(ob_ids[0]) 
             data = {'ob': ob}
             _id = ob['_id']
-            logging.info(f'sending ob {_id}')
+            logger.info(f'sending ob {_id}')
             emit('broadcast_submitted_ob_from_server', data, room=request.sid)
         except RuntimeError as err: 
             data = {'msg': f'{err}'}
@@ -68,7 +68,7 @@ def request_ob():
 @socketio.on("request_ee_state")
 def request_ee_state():
     """Sends OB stored on EE (first item in queue)"""
-    logging.info(f'request_ee_state event triggerd by {request.sid}')
+    logger.info(f'request_ee_state event triggerd by {request.sid}')
     submittedId = ee.obs_q.submitted_ob_id
     data = dict() 
     try: 
@@ -77,7 +77,7 @@ def request_ee_state():
             ob = ee.ODBInterface.get_OB_from_id(submittedId) 
             data['ob'] = ob
             _id = ob['_id']
-            logging.info(f'sending ob {_id}')
+            logger.info(f'sending ob {_id}')
         # get ob queue and ob boneyard
         ob_id_queue = ee.obs_q.get_ob_ids() 
         data['ob_id_queue'] = ob_id_queue 
@@ -91,10 +91,10 @@ def request_ee_state():
 
         evtBoneyard = [x.as_dict() for x in ee.ev_q.boneyard]
         data['event_boneyard'] = evtBoneyard 
-        logging.info('sending ee state to frontend')
+        logger.info('sending ee state to frontend')
         emit('broadcast_ee_state_from_server', data, room=request.sid)
     except RuntimeError as err: 
-        logging.warning(err)
+        logger.warning(err)
         data = {'msg': f'{err}'}
         emit('snackbar_msg', data, room=request.sid)
         return
@@ -102,7 +102,7 @@ def request_ee_state():
 @socketio.on("request_ob_queue")
 def request_ob_queue():
     """Sends list of selected OBs stored on disk"""
-    logging.info(f'sending ob_queue to {request.sid}')
+    logger.info(f'sending ob_queue to {request.sid}')
     ob_id_queue = ee.obs_q.get_ob_ids() 
     data = { 'ob_id_queue': ob_id_queue }
     emit('broadcast_ob_queue_from_server', data, room=request.sid)
@@ -119,10 +119,10 @@ def set_ob_queue(data):
             ee.magiq_interface.add_target_list_to_magiq(obs)
         except Exception as err:
             msg = f'did not add target to magiq. reason: {err}'
-            logging.warning(msg)
+            logger.warning(msg)
             data = { 'msg': msg}
             emit('snackbar_msg', data, room=request.sid)
-    logging.info(f'new ob queue len: {len(ob_ids)}')
+    logger.info(f'new ob queue len: {len(ob_ids)}')
 
     emit('broadcast_ob_queue_from_server', data, broadcast=True)
 
@@ -130,7 +130,7 @@ def set_ob_queue(data):
 def set_ob_boneyard(data):
     """Sets list of Selected OBs, stored on disk"""
     ob_ids = data.get('ob_id_boneyard')
-    logging.info(f'new ob queue boneyard len: {len(ob_ids)}')
+    logger.info(f'new ob queue boneyard len: {len(ob_ids)}')
     ee.obs_q.boneyard = ob_ids
     emit('broadcast_ob_boneyard_from_server', data, broadcast=True)
 
@@ -142,11 +142,11 @@ def sync_with_magiq(data):
 @socketio.on('submit_ob')
 def submit_ob(data):
     """Sets submitted OB to local storage, and sends it to execution engine and frontend."""
-    logging.info('submitting new ob from frontend')
+    logger.info('submitting new ob from frontend')
     submittedId = ee.obs_q.get().ob_id # gets first ob id and sends it to the boneyard 
     ee.obs_q.submitted_ob_id = submittedId
-    logging.info(f"submitted obid: {submittedId}")
-    logging.info(f"submitted obid matches? : {submittedId==data['ob_id']}")
+    logger.info(f"submitted obid: {submittedId}")
+    logger.info(f"submitted obid matches? : {submittedId==data['ob_id']}")
     try:
         ob = ee.ODBInterface.get_OB_from_id(submittedId) 
         broadcastData = { 'ob': ob }
@@ -156,7 +156,7 @@ def submit_ob(data):
     except RuntimeError as err:
         data = {'msg': str(err)}
         emit('snackbar_msg', data, room=request.sid)
-    logging.info("sending new obqueue and boneyard to clients")
+    logger.info("sending new obqueue and boneyard to clients")
     broadcastBoneyard = { 'ob_id_boneyard': [ x.ob_id for x in ee.obs_q.boneyard ] }
     ob_id_queue = ee.obs_q.get_ob_ids() 
     obQueueData = { 'ob_id_queue': ob_id_queue }
@@ -170,7 +170,7 @@ def new_sequence_queue(data):
     ob = data.get('ob')
     newSequenceQueue = [ SequenceItem(x, ob) for x in seqQueue ]
     ee.seq_q.set_queue(newSequenceQueue)
-    logging.info(f'new_sequence_queue len: {len(seqQueue)}')
+    logger.info(f'new_sequence_queue len: {len(seqQueue)}')
     emit('sequence_queue_broadcast', data, broadcast=True)
 
 @socketio.on('new_sequence_boneyard')
@@ -178,7 +178,7 @@ def new_sequence_boneyard(data):
     """Sets sequence queue boneyard to local storage, and sends it to execution engine and frontend"""
     seqBoneyard = data.get('sequence_boneyard')
     ob = data.get('ob')
-    logging.info(f'new sequence boneyard len {len(seqBoneyard)}')
+    logger.info(f'new sequence boneyard len {len(seqBoneyard)}')
     ee.seq_q.boneyard = [ SequenceItem(x, ob) for x in seqBoneyard ]
     emit('sequence_boneyard_broadcast', data, broadcast=True)
 
@@ -186,7 +186,7 @@ def new_sequence_boneyard(data):
 def new_event_queue(data):
     """Sets event queue to local storage, and sends it to execution engine and frontend"""
     eq = data.get('event_queue')
-    logging.info(f'new event queue len: {len(eq)}')
+    logger.info(f'new event queue len: {len(eq)}')
     newQueue = []
     oldQueue = [*ee.ev_q.queue]
     for eventStr in eq: #sorting in queue
@@ -203,7 +203,7 @@ def new_event_boneyard(data):
     """Sets event queue boneyard to local storage, and sends it to execution engine and frontend"""
     print('new event boneyard')
     eb = data.get('event_boneyard')
-    logging.info(f'new event boneyard len{len(eb)}')
+    logger.info(f'new event boneyard len{len(eb)}')
     newBoneyard = []
     oldBoneyard = [*ee.ev_q.boneyard]
     for eventDict in eb: # sorting in boneyard
@@ -222,7 +222,7 @@ def get_event(arr, eventDict):
         msg = f'CANNOT FIND {eventDict["script_name"]}'
         data = {'msg': msg}
         emit('snackbar_msg', data, room=request.sid)
-        logging.error(msg)
+        logger.error(msg)
     return evtItem 
 
 def build_list(arr, eventDicts):
@@ -265,7 +265,7 @@ def get_fresh_ob():
     # Get a fresh OB for the new task
     ob_id = ee.obs_q.submitted_ob_id 
     if len(ob_id) == 0:
-        logging.warning('ob queue empty')
+        logger.warning('ob queue empty')
         raise Exception('ob queue is empty')
     ob = ee.ODBInterface.get_OB_from_id(ob_id)
     return ob
@@ -275,7 +275,7 @@ def get_fresh_event(event):
     try:
         ob = get_fresh_ob()
     except Exception as err:
-        logging.warning('new_task error: {err}')
+        logger.warning('new_task error: {err}')
         data = {'msg': f'{err}'}
         emit('snackbar_msg', data, room=request.sid)
         return
@@ -297,31 +297,31 @@ def new_task(data):
     """Sets task to local storage, resets events to defaults (tbd: translator module replaces default events)
     resets event boneyard and broadcasts to frontend and execution engine"""
     task = data.get('task')
-    logging.info(f'new task getting set to {task}')
+    logger.info(f'new task getting set to {task}')
     isAcquisition = data.get('isAcq', False)
 
     try:
         ob = get_fresh_ob()
     except Exception as err:
-        logging.warning('new_task error: {err}')
+        logger.warning('new_task error: {err}')
         data = {'msg': f'{err}'}
         emit('snackbar_msg', data, room=request.sid)
         return
 
     if isAcquisition:
-        logging.info('acquisition getting set')
+        logger.info('acquisition getting set')
         acqSeq = ob.get('acquisition', False) 
         target = ob.get('target', False)
         if not acqSeq or not target:
             data = {'msg': 'no acquisition or target in ob'}
             emit('snackbar_msg', data, room=request.sid)
             return
-        logging.info(f"new acquistion task from queue {acqSeq['metadata']['script']}")
+        logger.info(f"new acquistion task from queue {acqSeq['metadata']['script']}")
         ee.ev_q.load_events_from_acquisition_and_target(ob)
     else: # is sequence
         seq_queue = [*ee.seq_q.queue]
         if len(seq_queue) == 0:
-            logging.warning('sequence queue empty')
+            logger.warning('sequence queue empty')
             data = {'msg': 'sequence queue empty'}
             emit('snackbar_msg', data, room=request.sid)
             return
@@ -337,7 +337,7 @@ def new_task(data):
             seqQueueData = { 'sequence_queue': ee.seq_q.get_sequences() }
             emit('sequence_boneyard_broadcast', seqBoneyardData, broadcast=True)
             emit('sequence_queue_broadcast', seqQueueData, broadcast=True)
-            logging.info(f'new sequence from queue {newSeqItem.sequence}')
+            logger.info(f'new sequence from queue {newSeqItem.sequence}')
             ee.ev_q.load_events_from_sequence(newSeqItem)
     ee.ev_q.boneyard = []
     outData = make_event_out_data()
@@ -351,25 +351,25 @@ def submit_event(data):
     eventDict = data.get('submitted_event')
     eventDict = get_fresh_event(eventDict) # Retrieve most recent OB data
 
-    logging.info(f'submitting event {eventDict["script_name"]}')
+    logger.info(f'submitting event {eventDict["script_name"]}')
 
     if len(ee.ev_q.get_queue_as_list()) == 0: 
-        logging.warning('event queue empty')
+        logger.warning('event queue empty')
         data = { 'msg': 'event queue empty'}
         emit('snackbar_msg', data, room=request.sid)
         return
     #aquire lock 
     isBlocked = ee.ev_q.block_event.is_set()
     if isBlocked:
-        logging.warning('queue locked. sending msg to frontend')
+        logger.warning('queue locked. sending msg to frontend')
         data = { 'msg': 'event queue locked'}
         emit('snackbar_msg', data, room=request.sid)
         return
     try:    
         ee.ev_q.dispatch_event(eventDict)
     except Exception as err:
-        logging.info(f'dispatch event failed, reason: {err}')
-        logging.info(f'{traceback.format_exc()}')
+        logger.info(f'dispatch event failed, reason: {err}')
+        logger.info(f'{traceback.format_exc()}')
         data = { 'msg': 'dispatch_event failed' }
     # broadcast new queue and boneyard
     outData = make_event_out_data()
@@ -377,7 +377,7 @@ def submit_event(data):
 
 @socketio.on('release_event_queue_lock')
 def release_event_queue_lock():
-    logging.info('release_event_queue_lock event triggered')
+    logger.info('release_event_queue_lock event triggered')
     ee.ev_q.block_event.clear()
     ee.server_connection.send({"message": "release_event_queue_lock"})
         
