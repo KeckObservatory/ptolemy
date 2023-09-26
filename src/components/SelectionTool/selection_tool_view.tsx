bae570796cca5716982fbf3ba5e6ac654c9c5fba
Tyler Tucker <tylertucker202@gmail.com>
2023-09-26 10:08:48 -1000

    remove ping pong

--- src/components/SelectionTool/selection_tool_view.tsx
+++ src/components/SelectionTool/selection_tool_view.tsx
@@ -138,21 +138,6 @@ export const SelectionToolView = (props: Props) => {
 
         console.log('creating connections')
 
-        window.setInterval(function () {
-            start_time = (new Date).getTime();
-            socket.emit('my_ping');
-        }, 1000);
-
-        socket.on('my_pong', function () {
-            var latency = new Date().getTime() - start_time;
-            ping_pong_times.push(latency);
-            ping_pong_times = ping_pong_times.slice(-30); // keep last 30 samples
-            var sum = 0;
-            for (var i = 0; i < ping_pong_times.length; i++)
-                sum += ping_pong_times[i];
-            setAvg(Math.round(10 * sum / ping_pong_times.length) / 10)
-        });
-
         socket.on('broadcast_ob_queue_from_server', set_ob_queue_from_server)
 
         socket.on('broadcast_submitted_ob_from_server', set_ob_from_server)
