1679a9da3df3fc8e4143ad8b0a58caad896e5baa
Tyler Tucker <tylertucker202@gmail.com>
2023-09-26 09:40:36 -1000

    trying to reduce rerendering

--- src/components/SelectionTool/selected_ob_table.tsx
+++ src/components/SelectionTool/selected_ob_table.tsx
@@ -178,84 +178,85 @@ const SelectedOBTable = (props: Props) => {
         // socket.emit('set_ob_boneyard', { ob_id_boneyard: boneyardIds })
     }
 
-    const options: MUIDataTableOptions = {
-        filterType: 'dropdown',
-        onRowsDelete: () => false,
-        expandableRows: true,
-        renderExpandableRow: (rowData, rowMeta: { dataIndex: number, rowIndex: number }) => {
-            const ob = obs[rowMeta.dataIndex]
-            const colSpan = rowData.length + 1;
-            return (
-                <TableRow>
-                    <TableCell colSpan={colSpan}>
-                        <ReactJson
-                            src={ob as object}
-                            theme={jsontheme as ThemeKeys}
-                            collapsed={true}
-                            enableClipboard={true}
-                            onEdit={false}
-                        />
-                    </TableCell>
-                </TableRow >
-            )
-        },
-        isRowSelectable: (dataIndex: number, selectedRows: MUIDataTableIsRowCheck | undefined) => {
-            return !rows[dataIndex].completed
-        },
-        selectableRowsHeader: false,
-        selectableRowsHideCheckboxes: false,
-        customToolbarSelect: selectedRows => {
-            const selRow = rows[selectedRows.data[0].dataIndex]
-            const idx = props.selOBs.findIndex((ob: ObservationBlock) => ob._id === selRow.ob_id)
-            return (
-                <OBToolbarSelect
-                    idx={idx}
-                    selOBs={props.selOBs}
-                    setSelOBs={props.setSelOBs}
-                    onSubmitOB={props.onSubmitOB}
-                />)
-        },
-        selectableRows: 'single'
-    }
+    // const options: MUIDataTableOptions = {
+    //     filterType: 'dropdown',
+    //     onRowsDelete: () => false,
+    //     expandableRows: true,
+    //     renderExpandableRow: (rowData, rowMeta: { dataIndex: number, rowIndex: number }) => {
+    //         const ob = obs[rowMeta.dataIndex]
+    //         const colSpan = rowData.length + 1;
+    //         return (
+    //             <TableRow>
+    //                 <TableCell colSpan={colSpan}>
+    //                     <ReactJson
+    //                         src={ob as object}
+    //                         theme={jsontheme as ThemeKeys}
+    //                         collapsed={true}
+    //                         enableClipboard={true}
+    //                         onEdit={false}
+    //                     />
+    //                 </TableCell>
+    //             </TableRow >
+    //         )
+    //     },
+    //     isRowSelectable: (dataIndex: number, selectedRows: MUIDataTableIsRowCheck | undefined) => {
+    //         return !rows[dataIndex].completed
+    //     },
+    //     selectableRowsHeader: false,
+    //     selectableRowsHideCheckboxes: false,
+    //     customToolbarSelect: selectedRows => {
+    //         const selRow = rows[selectedRows.data[0].dataIndex]
+    //         const idx = props.selOBs.findIndex((ob: ObservationBlock) => ob._id === selRow.ob_id)
+    //         return (
+    //             <OBToolbarSelect
+    //                 idx={idx}
+    //                 selOBs={props.selOBs}
+    //                 setSelOBs={props.setSelOBs}
+    //                 onSubmitOB={props.onSubmitOB}
+    //             />)
+    //     },
+    //     selectableRows: 'single'
+    // }
 
-    const columns = [
-        { name: 'ob_id', label: 'OB ID', options: { display: false } },
-        { name: 'name', label: 'OB Name', options: {} },
-        { name: 'tgt_name', label: 'Target Name', options: {} },
-        { name: 'version', label: 'Version', options: { display: false } },
-        { name: 'comment', label: 'Comment', options: { display: false } },
-        { name: 'ra', label: 'RA', options: { display: true } },
-        { name: 'dec', Label: 'Dec', options: { display: true } },
-        { name: 'sem_id', Label: 'Semid', options: { display: false } },
-        {
-            name: 'completed',
-            label: 'Completed',
-            options: {
-                display: true,
-                customBodyRender: (value: boolean, tableMeta: any, updateValue: any) => {
-                    return (
-                        <FormControlLabel
-                            label=""
-                            value={value}
-                            control={<Switch checked={value} />}
-                            onChange={(_, checked) => update_value(value, checked, tableMeta)}
-                        />
-                    )
+    // const columns = [
+    //     { name: 'ob_id', label: 'OB ID', options: { display: false } },
+    //     { name: 'name', label: 'OB Name', options: {} },
+    //     { name: 'tgt_name', label: 'Target Name', options: {} },
+    //     { name: 'version', label: 'Version', options: { display: false } },
+    //     { name: 'comment', label: 'Comment', options: { display: false } },
+    //     { name: 'ra', label: 'RA', options: { display: true } },
+    //     { name: 'dec', Label: 'Dec', options: { display: true } },
+    //     { name: 'sem_id', Label: 'Semid', options: { display: false } },
+    //     {
+    //         name: 'completed',
+    //         label: 'Completed',
+    //         options: {
+    //             display: true,
+    //             customBodyRender: (value: boolean, tableMeta: any, updateValue: any) => {
+    //                 return (
+    //                     <FormControlLabel
+    //                         label=""
+    //                         value={value}
+    //                         control={<Switch checked={value} />}
+    //                         onChange={(_, checked) => update_value(value, checked, tableMeta)}
+    //                     />
+    //                 )
 
-                }
-            }
-        }
-    ]
+    //             }
+    //         }
+    //     }
+    // ]
 
-    return ( rows.length>0 ? 
-        (<MUIDataTable
-            data={rows}
-            columns={columns}
-            options={options}
-            title={"Selected OBs"}
-            components={{ Checkbox: CustomCheckbox }}
-        />) : (<React.Fragment></React.Fragment>)
-    )
+    // return ( rows.length>0 ? 
+    //     (<MUIDataTable
+    //         data={rows}
+    //         columns={columns}
+    //         options={options}
+    //         title={"Selected OBs"}
+    //         components={{ Checkbox: CustomCheckbox }}
+    //     />) : (<React.Fragment></React.Fragment>)
+    // )
+    return <React.Fragment></React.Fragment>
 }
 
 export default SelectedOBTable
