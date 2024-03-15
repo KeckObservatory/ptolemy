35baaa373b92c0cb2dda0729c09bf58863ad5297
Tyler Tucker <tylertucker202@gmail.com>
2024-03-14 14:05:56 -1000

    for ws connection

--- src/contexts/socket.tsx
+++ src/contexts/socket.tsx
@@ -3,7 +3,7 @@ import {io} from "socket.io-client";
 import { RWindow } from '../typings/ptolemy';
 
 declare let window: RWindow
-const url = window.BASE_URL;
+const url = window.BASE_URL + ':50008';
 const path = '';
 export const socket = io(url, {path: path, transports: ['websocket'], secure: true, rejectUnauthorized: false});
 export const SocketContext = React.createContext(socket);
